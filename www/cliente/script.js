1d5fc4f15a6a46fdb5dd6e976aed2867b9039e67
martin <100472279@alumnos.uc3m.es>
2024-04-14 20:50:55 +0200

    primera version reconocimeinto de imagenes

--- www/cliente/script.js
+++ www/cliente/script.js
@@ -37,8 +37,13 @@ const mapa = document.getElementById("mostrar-mapa");
 const botonMapa = document.getElementById("boton-mapa");
 const botonCentrarMapa = document.getElementById("boton-centrar-mapa");
 
+const escaneandoCamara = document.getElementById("escaneando-imagen");
+const botonCamara = document.getElementById("boton-camara");
+const botonRetrocederCamara = document.getElementById("boton-retroceder-camara");
+
 // Ocultamos la sección de escaneando sonido al cargar la página
 escaneandoSonido.style.display = "none";
+escaneandoCamara.style.display = "none"; // No se porque no funciona bien, no lo pone a none
 
 // Ocultar el mapa al cargar la página
 mapa.style.display = "none";
@@ -50,12 +55,25 @@ let mymap = null;
 botonSonido.addEventListener("touchstart", function() {
   pantallaPrincipal.style.display = "none";
   escaneandoSonido.style.display = "block";
+  escaneandoCamara.style.display = "none";
 });
 
 // Función para manejar el evento táctil en el botón de retroceso
 botonRetroceder.addEventListener("touchstart", function() {
   pantallaPrincipal.style.display = "block";
   escaneandoSonido.style.display = "none";
+});
+
+// Función para manejar el evento táctil en el botón de camara
+botonCamara.addEventListener("touchstart", function() {
+  pantallaPrincipal.style.display = "none";
+  escaneandoCamara.style.display = "block";
+});
+
+// Función para manejar el evento táctil en el botón de retroceso
+botonRetrocederCamara.addEventListener("touchstart", function() {
+  pantallaPrincipal.style.display = "block";
+  escaneandoCamara.style.display = "none";
 });
 
 // Función para manejar el evento táctil en el botón de retroceso en el mapa
@@ -69,6 +87,7 @@ botonMapa.addEventListener("touchstart", function() {
   mapa.style.display = "block"; // Mostrar el contenedor del mapa
   pantallaPrincipal.style.display = "none"; // Ocultar pantalla principal
   escaneandoSonido.style.display = "none";
+  escaneandoCamara.style.display = "none";
 });
 
 // Definir el ícono personalizado
@@ -197,9 +216,6 @@ function reconocerPalabra(palabra) {
   }
 }
 
-
-
-
 // Función para iniciar el reconocimiento de voz
 function iniciarReconocimientoVoz() {
   const recognition = new webkitSpeechRecognition() || new SpeechRecognition();
@@ -219,4 +235,61 @@ const botonGrabar = document.getElementById("boton-pago");
 
 // Agregar evento al botón para iniciar el reconocimiento de voz
 botonGrabar.addEventListener("touchstart", iniciarReconocimientoVoz);
+
+
+////////////////////////// Reconocimiento de imagenes //////////////////////////
+
+// extraemos la url de nuestro modelo, lo holdea google y funciona 
+const URL = "https://teachablemachine.withgoogle.com/models/RZs7GMDB5/";
+
+let model, webcam, labelContainer, maxPredictions;
+let predictionsList = []; // Declara una lista para almacenar las predicciones
+
+// Cargamos el modelo y las imágenes
+async function init() {
+    // Definimos las URLs del modelo y los metadatos
+    const modelURL = URL + "model.json"; // URL del archivo model.json
+    const metadataURL = URL + "metadata.json"; // URL del archivo metadata.json
+
+    // Cargamos el modelo y los metadatos
+    model = await tmImage.load(modelURL, metadataURL); // Cargamos el modelo y los metadatos desde las URLs definidas
+    maxPredictions = model.getTotalClasses(); // Obtenemos el número total de clases del modelo
+
+    // Configuramos la cámara web
+    const flip = true; // Indica si se debe voltear la cámara web
+        webcam = new tmImage.Webcam(200, 200, flip); // Creamos una instancia de la clase Webcam con un tamaño de 200x200 y volteo activado
+        await webcam.setup(); // Solicitamos acceso a la cámara web y la inicializamos
+        await webcam.play(); // Comenzamos a reproducir el flujo de vídeo de la cámara web
+        window.requestAnimationFrame(loop); // Iniciamos el bucle de actualización de la cámara web
+
+    // Añadimos elementos al DOM para mostrar la vista previa de la cámara y las etiquetas de clase
+    document.getElementById("webcam-container").appendChild(webcam.canvas); // Añadimos el lienzo de la cámara al contenedor correspondiente en el HTML
+    labelContainer = document.getElementById("label-container"); // Obtenemos el contenedor de etiquetas
+    for (let i = 0; i < maxPredictions; i++) { // Creamos un div por cada etiqueta de clase
+        labelContainer.appendChild(document.createElement("div"));
+    }
+    // La camara esta siempre activa y se actualiza en cada frame, no se hace una foto como tal. 
+}
+// La función loop() se llama inicialmente al iniciar el programa, y luego se llama repetidamente a través de window.requestAnimationFrame(loop).
+async function loop() {
+    webcam.update(); // haces update de la camara
+    await predict();
+    window.requestAnimationFrame(loop);
+    }
+
+    // Realiza una predicción con el modelo Teachable Machine y almacena las predicciones en una lista y las manda al container
+async function predict() {
+    const prediction = await model.predict(webcam.canvas);
+    let predictions = []; // Lista temporal para almacenar las predicciones de este fotograma
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+                prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+            labelContainer.childNodes[i].innerHTML = classPrediction; // Para meterlo en el container
+
+        predictions.push(classPrediction); // Agrega la predicción a la lista temporal
+    }
+    predictionsList.push(predictions); // Agrega la lista de predicciones de este fotograma a la lista principal
+}
+
+
 
